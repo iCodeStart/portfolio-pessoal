a8039baa7c8cbe81da1851d76e11e36779114403
Alan Braulio <alanbraulio@ciandt.com>
2023-09-08 20:44:35 -0300

    adjustments responsive code

--- script.js
+++ script.js
@@ -115,3 +115,43 @@ document.addEventListener("DOMContentLoaded", function(){
         })
     })
 })
+
+// Scroll
+
+function scrollToSection(sectionId) {
+    const section = document.querySelector(sectionId);
+    if (section) {
+        let scrollOffset = 0;
+
+        if (sectionId === '#projects') {
+            scrollOffset = section.offsetTop - 70;
+        } else {
+            scrollOffset = section.offsetTop - (window.innerHeight - section.clientHeight) / 2;
+        }
+
+        window.scrollTo({
+            top: scrollOffset,
+            behavior: 'smooth'
+        });
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const links = document.querySelectorAll('nav a');
+    links.forEach(function (link) {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+            const sectionId = link.getAttribute('href');
+            scrollToSection(sectionId);
+        });
+    });
+
+    const footerLinks = document.querySelectorAll('footer a');
+    footerLinks.forEach(function (link) {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+            const sectionId = link.getAttribute('href');
+            scrollToSection(sectionId);
+        });
+    });
+});
